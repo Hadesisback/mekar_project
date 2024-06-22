22cd1a4b8b51c34175102bfcf18586ca73131213
Febrian K <febrian911@gmail.com>
2024-06-23 05:45:33 +0700

    update more

--- frontend/src/lib/env.mjs
+++ frontend/src/lib/env.mjs
@@ -7,27 +7,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    DATABASE_URL: z.string().min(1),
     BACKEND_ENDPOINT:z.string().min(1),
     
-    NEXTAUTH_SECRET: process.env.NODE_ENV === "production"
-        ? z.string().min(1)
-        : z.string().min(1).optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL_URL ? z.string().min(1) : z.string().url()
-    ),
-    DISCORD_CLIENT_ID: z.string().min(1),
-    DISCORD_CLIENT_SECRET: z.string().min(1),
-    GOOGLE_CLIENT_ID: z.string().min(1),
-    GOOGLE_CLIENT_SECRET: z.string().min(1),
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_CLIENT_SECRET: z.string().min(1),
-    APPLE_CLIENT_ID: z.string().min(1),
-    APPLE_CLIENT_SECRET: z.string().min(1),
   },
   client: {
     
