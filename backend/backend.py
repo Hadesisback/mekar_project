5b473c5b696aa4be046756c7f33bc4e3cf4db3e8
Febrian K <febrian911@gmail.com>
2024-06-24 07:18:33 +0700

    Update docker workflow, unittest

--- backend/backend.py
+++ backend/backend.py
@@ -4,8 +4,8 @@ from pymongo import MongoClient
 from flask_cors import CORS, cross_origin 
 from dotenv import load_dotenv
 from tabulate import tabulate
+import re
 
-# Load environment variables from .env file
 load_dotenv()
 
 def run_app():
@@ -28,9 +28,18 @@ def run_app():
         date_of_birth = data.get('date_of_birth')
         email = data.get('email')
 
+        if not all([name, identity_number, date_of_birth, email]):
+            return jsonify({"error": "All fields must be provided."}), 400
         
+        if not re.match(r'^[a-zA-Z\s]+$', name):
+            return jsonify({"error": "Name must contain only alphabets."}), 400
+
         if not identity_number.isdigit() or len(identity_number) != 16:
             return jsonify({"error": "Identity number must be 16 digits and all numerical."}), 400
+
+        dob_pattern = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+        if not dob_pattern.match(date_of_birth):
+            return jsonify({"error": "Date of birth must be in the format YYYY-MM-DD."}), 400
         
         # Check if the 7th to 12th digits of identity number match the date of birth
         id_date_part = identity_number[6:12]
@@ -40,7 +49,6 @@ def run_app():
         if id_date_part != dob_format:
             return jsonify({"error": "Identity number does not match the date of birth."}), 400
         
-        # Check for duplicate entries in the database
         existing_entry = collection.find_one({
             "name": name,
             "identity_number": identity_number,
@@ -51,7 +59,6 @@ def run_app():
         if existing_entry:
             return jsonify({"message": "Duplicate entry detected."}), 200
         
-        # Insert data into the database
         collection.insert_one({
             "name": name,
             "identity_number": identity_number,
@@ -70,7 +77,6 @@ def run_app():
         if not data:
             return jsonify({"message": "No data found in the database."}), 200
         
-        # Convert the data to a list of lists for tabulate
         table_data = [list(item.values()) for item in data]
         headers = ["Name", "Identity Number", "Date of Birth", "Email"]
         table = tabulate(table_data, headers, tablefmt="plain")
@@ -79,6 +85,7 @@ def run_app():
     
 
     return app
+
 if __name__ == '__main__':
     app = run_app()
     listen_host = os.getenv('LISTEN_HOST', '0.0.0.0')
